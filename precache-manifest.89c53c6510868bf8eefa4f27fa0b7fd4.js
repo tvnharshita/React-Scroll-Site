d2f9175e8c0387826a85c0b86528ac364671fb9d
Harshita <harsh@harshita>
2020-05-12 21:32:08 +0530

    Updates

---
+++ precache-manifest.89c53c6510868bf8eefa4f27fa0b7fd4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488861272afa8cb8276260e487e6e9de",
+    "url": "/react-scroll-site/index.html"
+  },
+  {
+    "revision": "ab79ff55c9d04ba0328a",
+    "url": "/react-scroll-site/static/css/main.3a8cd3c8.chunk.css"
+  },
+  {
+    "revision": "475b748226232f00ef97",
+    "url": "/react-scroll-site/static/js/2.e2ac52ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-scroll-site/static/js/2.e2ac52ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab79ff55c9d04ba0328a",
+    "url": "/react-scroll-site/static/js/main.3a9c5f57.chunk.js"
+  },
+  {
+    "revision": "34c1e56bd1a5b96de224",
+    "url": "/react-scroll-site/static/js/runtime-main.1f4ad416.js"
+  },
+  {
+    "revision": "658407062df475185985f9b2ba444648",
+    "url": "/react-scroll-site/static/media/Art.65840706.jpg"
+  },
+  {
+    "revision": "9f7a475d761498671543da64ea7f325c",
+    "url": "/react-scroll-site/static/media/Cooking1.9f7a475d.jpg"
+  },
+  {
+    "revision": "1eccaae748e0e9794f3e80e0ac851878",
+    "url": "/react-scroll-site/static/media/HomePageImage.1eccaae7.jpg"
+  },
+  {
+    "revision": "134dcf61dcc5f11435726ffe1da19150",
+    "url": "/react-scroll-site/static/media/IntroImage.134dcf61.jpg"
+  },
+  {
+    "revision": "06b6deb1f4e49694a455f094d1a0467c",
+    "url": "/react-scroll-site/static/media/Travel3.06b6deb1.jpg"
+  },
+  {
+    "revision": "60ecb224cf85ff702f28ce243ebb2507",
+    "url": "/react-scroll-site/static/media/needs.60ecb224.jpg"
+  },
+  {
+    "revision": "f927ad4979338ae2c4a71169756f8aad",
+    "url": "/react-scroll-site/static/media/share.f927ad49.jpg"
+  }
+]);
